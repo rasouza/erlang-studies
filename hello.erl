b67553ec8471057a51b5f50028f336f2895d2921
Rodrigo Souza <rodrigo.souza@sumup.com>
2018-04-12 16:15:02 -0300

    atoms

--- hello.erl
+++ hello.erl
@@ -1,7 +1,10 @@
 -module(hello).
--export([double/1, fac/1]).
+-export([double/1, fac/1, convert/2]).
 
 double(X) -> 2*X.
 
 fac(1) -> 1;
 fac(N) -> N * fac(N-1).
+
+convert(M, inc) -> M / 2.54;
+convert(N, centimeter) -> N * 2.54.
